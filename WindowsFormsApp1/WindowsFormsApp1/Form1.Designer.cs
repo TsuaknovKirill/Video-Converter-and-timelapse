551b95c2660d5af114859286bccff825ca74a330
TsuaknovKirill <vampir695@gmail.com>
2019-04-03 17:36:29 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_audio = new System.Windows.Forms.Button();
+            this.button_Openfile = new System.Windows.Forms.Button();
+            this.OpenFileVideo = new System.Windows.Forms.OpenFileDialog();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button_kadr = new System.Windows.Forms.Button();
+            this.mesto = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.tbVideo = new System.Windows.Forms.TextBox();
+            this.tboutdir = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_audio
+            // 
+            this.button_audio.Location = new System.Drawing.Point(28, 147);
+            this.button_audio.Name = "button_audio";
+            this.button_audio.Size = new System.Drawing.Size(263, 96);
+            this.button_audio.TabIndex = 0;
+            this.button_audio.Text = "Извлечь аудио";
+            this.button_audio.UseVisualStyleBackColor = true;
+            this.button_audio.Click += new System.EventHandler(this.button_audio_Click);
+            // 
+            // button_Openfile
+            // 
+            this.button_Openfile.Location = new System.Drawing.Point(8, 42);
+            this.button_Openfile.Name = "button_Openfile";
+            this.button_Openfile.Size = new System.Drawing.Size(297, 59);
+            this.button_Openfile.TabIndex = 1;
+            this.button_Openfile.Text = "Открыть фаил";
+            this.button_Openfile.UseVisualStyleBackColor = true;
+            this.button_Openfile.Click += new System.EventHandler(this.button_Openfile_Click);
+            // 
+            // OpenFileVideo
+            // 
+            this.OpenFileVideo.FileName = "OpenFileVideo";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, -2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(324, 303);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button_audio);
+            this.tabPage1.Controls.Add(this.button_Openfile);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(316, 277);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Video";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.tboutdir);
+            this.tabPage2.Controls.Add(this.tbVideo);
+            this.tabPage2.Controls.Add(this.start);
+            this.tabPage2.Controls.Add(this.mesto);
+            this.tabPage2.Controls.Add(this.button_kadr);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(316, 277);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Photo";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button_kadr
+            // 
+            this.button_kadr.Location = new System.Drawing.Point(73, 29);
+            this.button_kadr.Name = "button_kadr";
+            this.button_kadr.Size = new System.Drawing.Size(175, 39);
+            this.button_kadr.TabIndex = 0;
+            this.button_kadr.Text = "Выбрать файил";
+            this.button_kadr.UseVisualStyleBackColor = true;
+            this.button_kadr.Click += new System.EventHandler(this.button_kadr_Click_1);
+            // 
+            // mesto
+            // 
+            this.mesto.Location = new System.Drawing.Point(73, 100);
+            this.mesto.Name = "mesto";
+            this.mesto.Size = new System.Drawing.Size(175, 41);
+            this.mesto.TabIndex = 1;
+            this.mesto.Text = "Указать место";
+            this.mesto.UseVisualStyleBackColor = true;
+            this.mesto.Click += new System.EventHandler(this.mesto_Click);
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(6, 184);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(299, 75);
+            this.start.TabIndex = 2;
+            this.start.Text = "СТАРТ!";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // tbVideo
+            // 
+            this.tbVideo.Location = new System.Drawing.Point(6, 74);
+            this.tbVideo.Name = "tbVideo";
+            this.tbVideo.Size = new System.Drawing.Size(299, 20);
+            this.tbVideo.TabIndex = 10;
+            this.tbVideo.TextChanged += new System.EventHandler(this.tbVideo_TextChanged);
+            // 
+            // tboutdir
+            // 
+            this.tboutdir.Location = new System.Drawing.Point(6, 158);
+            this.tboutdir.Name = "tboutdir";
+            this.tboutdir.Size = new System.Drawing.Size(299, 20);
+            this.tboutdir.TabIndex = 11;
+            this.tboutdir.TextChanged += new System.EventHandler(this.tboutdir_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 291);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_audio;
+        private System.Windows.Forms.Button button_Openfile;
+        private System.Windows.Forms.OpenFileDialog OpenFileVideo;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button_kadr;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button mesto;
+        private System.Windows.Forms.TextBox tbVideo;
+        private System.Windows.Forms.TextBox tboutdir;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NReco.VideoConverter;
+using System.Net;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        string InputFile;
+        string OutputFile;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+
+        private void button_Openfile_Click(object sender, EventArgs e)
+        {
+
+
+           if (OpenFileVideo.ShowDialog() == DialogResult.OK)
+            {
+                InputFile = OpenFileVideo.FileName;
+                OutputFile = InputFile.Substring(0, InputFile.IndexOf("."));
+
+            }
+
+    
+        }
+
+
+        private void button_audio_Click(object sender, EventArgs e)
+        {
+            var ConvertVideo = new NReco.VideoConverter.FFMpegConverter();
+            ConvertVideo.ConvertMedia(InputFile, OutputFile + ".mp3", "mp3");
+
+
+
+        }
+
+
+
+        private void button_kadr_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                tbVideo.Text = ofd.FileName;
+            }
+        }
+
+        private void mesto_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                tboutdir.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            string inputPath = tbVideo.Text;
+            string outputPath = tboutdir.Text;
+
+            try
+            {
+
+                if (inputPath != null && outputPath != null)
+                {
+                    float duration = GetVideoDuration(inputPath);
+                    FFMpegConverter ffmegc = new FFMpegConverter();
+                    float frameTime = 2.9f;
+                    int counter = 1;
+                    while (frameTime <= duration)
+                    {
+                        string img = Path.Combine(outputPath, string.Format("frame{0}.jpg", counter));
+                        ffmegc.GetVideoThumbnail(inputPath, img, frameTime);
+                        counter++;
+                        frameTime += 2.9f;
+                        
+                        
+                    }
+                }
+            }
+            catch (FFMpegException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private float GetVideoDuration(string videosource)
+        {
+            var ffProbe = new NReco.VideoInfo.FFProbe();
+            var videoInfo = ffProbe.GetMediaInfo(videosource);
+            double duration = videoInfo.Duration.TotalSeconds;
+            float f_duration = (float)duration;
+            return f_duration;
+        }
+
+        public static Image resizeImage(Image imgToResize, Size size)
+        {
+            return (Image)(new Bitmap(imgToResize, size));
+        }
+
+        private void tbVideo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tboutdir_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
